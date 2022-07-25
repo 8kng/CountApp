4e87b205118fcdb86cc45e0582016aa9c33427f2
8kengo <mi191324@edu.okinawa-ct.ac.jp>
2022-07-25 15:36:27 +0900

    アプリの実装

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.counterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.example.counterapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.countTxt)
+        val btn = findViewById<Button>(R.id.countBtn)
+        btn.setOnClickListener {
+            count += 1
+            text.text = count.toString()
+        }
     }
 }
